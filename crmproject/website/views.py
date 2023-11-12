1b343154581ad040a81059ccbcdd8d799f7214f0
vinodsagar260981 <vinodsagar0981@gmail.com>
2023-11-12 07:48:50 +0530

    third

--- crmproject/website/views.py
+++ crmproject/website/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    #check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Logged in successfully")
+            return redirect('home')
+        else:
+            messages.success(request, "Please check Credentials")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have logout successfully!")
+    return redirect('home')
